e8543d960e405fa793d9aacc8f9183ac650b0583
Christos Falas <raspberrymaker@gmail.com>
2020-04-11 10:18:06 +0300

    Added messenger stuff

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -387,3 +387,39 @@ def cart_order(request, user):
 			o.save()
 		cart = ShoppingItem.objects.filter(UserID=user).delete()		
 		return HttpResponse(status=200)
+
+@api_view(['POST'])
+def messenger(request, *args, **kwargs):
+	if request.method=='POST':
+		# Converts the text payload into a python dictionary
+		incoming_message = json.loads(request.body.decode('utf-8'))
+		# Facebook recommends going through every entry since they might send
+		# multiple messages in a single call during high load
+		for entry in incoming_message['entry']:
+			for message in entry['messaging']:
+				# Check to make sure the received call is a message call
+				# This might be delivery, optin, postback for other events 
+				if 'message' in message:
+					# Print the message to the terminal
+					print(message)    
+					# Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
+					# are sent as attachments and must be handled accordingly. 
+					post_facebook_message(message['sender']['id'], message['message']['text'])    
+	else:
+		if request.GET['hub.verify_token'] == "communityhero":
+			return HttpResponse(request.GET['hub.challenge'])
+		else:
+			return HttpResponse('Error, invalid token')
+	return HttpResponse()    
+
+def post_facebook_message(fbid, recevied_message):
+	# Remove all punctuations, lower case the text and split it based on space
+	PAGE_ACCESS_TOKEN = 'EAAj0jNiswxgBAO3xduZATZAFZAyWGlzT3ZB505szUr3vINC2cpaEtm3K2wGQztnPEZAG07ZAijSm9csUZARKKUHWb0TRyIoGvCeIAmZAZAOH3blI15hlu7nhQ5ROyvxlqIi2RVbRGSJi4rY6g1eZANUZCHSKLTZBrNq4RfZCP88oZA7p8A4ClEPYU8h7Be'
+	user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
+	user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
+	user_details = requests.get(user_details_url, user_details_params).json() 
+	joke_text = 'Yo '+user_details['first_name']+'..! ' + "Hello!"
+	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
+	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+	pprint(status.json())
