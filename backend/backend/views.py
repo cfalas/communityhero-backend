99193cda6713a9dbab37d51b0c7e3538049cfdd2
Christos Falas <raspberrymaker@gmail.com>
2020-04-26 23:56:47 +0300

    Added new url for SMS

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -289,7 +289,7 @@ def chatbot(request):
 						r['content']+=b['content'].split('\n')[item] + ": " + req['items'][item] + '\n'
 					else:
 						r['content']+=req['items'][item] + '\n'
-				r['content']+='That would cost you a total of €' + str(req['cost']) + '\nYou can edit or complete your order here: http://192.168.30.179/wordpress/index.php/cart/?fill_cart='
+				r['content']+='That would cost you a total of €' + str(req['cost']) + '\nYou can edit or complete your order here: https://communityhero.me/index.php/cart/?fill_cart='
 				for item in range(len(req['itemsWordpress'])):
 					r['content']+=str(req['itemsWordpress'][item])
 					if item!=len(req['items'])-1:
